e191abbbc19c1f239976c5b6ea8ff5000cf19d7c
rchesaux <chesaux66@gmail.com>
2024-11-06 10:29:25 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("workin")
+
+function Recipie(name, email, ingredients) {
+    this.name = name;
+    this.email = email;
+    this.ingredients = ingredients;
+}
+
+const recipie1 = new Recipie(document.getElementById())
+
+const saveForm = () => {
+
+}
